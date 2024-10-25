503c93e92877f1c412f258292475ed9bc1646719
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2024-10-25 06:38:30 -0300

    refactor(index): muda o diretorio dos arquivos index

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -5,16 +9,16 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="home.css">
+        <link rel="stylesheet" href="index.css">
         <title> Home </title>
 
-        <link rel="stylesheet" href="../MenuFooter/footer/footer.css">
-        <link rel="stylesheet" href="../MenuFooter/menu/menu.css">
-        <link rel="stylesheet" href="../Home/reponsive-home.css">
+        <link rel="stylesheet" href="MenuFooter/footer/footer.css">
+        <link rel="stylesheet" href="MenuFooter/menu/menu.css">
+        <link rel="stylesheet" href="index-responsive.css">
     </head>
     <body>
         <div id="geral">
-            <?php include '../MenuFooter/menu/menu.php';?>
+            <?php include 'MenuFooter/menu/menu.php';?>
 
             
 
@@ -37,15 +41,15 @@
 
                                 <div class="input-field">
                                     <input type="email" name="email" placeholder="Email" required maxlength="35">
-                                    <img src="../LoginCadastro/img/email.png" alt="" class="cadastrar-icons" id="email-icon">
+                                    <img src="LoginCadastro/img/email.png" alt="" class="cadastrar-icons" id="email-icon">
                                 </div>
 
                                 <div class="input-field">
                                     <input type="password" id="password" name="senha" placeholder="Senha" required maxlength="23">
-                                    <img src="../LoginCadastro/img/senha.png" alt="" class="cadastrar-icons" id="senha-icon">
+                                    <img src="LoginCadastro/img/senha.png" alt="" class="cadastrar-icons" id="senha-icon">
 
-                                    <img src="../LoginCadastro/img/esconder-senha.png" alt="" id="esconder-senha" class="esconder-mostrar-senha" onclick="senhaMostrar()">
-                                    <img src="../LoginCadastro/img/mostrar-senha.png" alt="" id="mostrar-senha" class="esconder-mostrar-senha" onclick="senhaMostrar()">
+                                    <img src="LoginCadastro/img/esconder-senha.png" alt="" id="esconder-senha" class="esconder-mostrar-senha" onclick="senhaMostrar()">
+                                    <img src="LoginCadastro/img/mostrar-senha.png" alt="" id="mostrar-senha" class="esconder-mostrar-senha" onclick="senhaMostrar()">
 
 
                                     <script>
@@ -69,11 +73,11 @@
                                     </script>
                                 </div>
 
-                                <a href="../LoginCadastro/cadastro.php"> <p> Não possui um conta? </p> </a>
+                                <a href="LoginCadastro/cadastro.php"> <p> Não possui um conta? </p> </a>
                                 
                                 <div id="mensagem-box">
                                     <?php
-                                        include('../BancoDeDados/conexao.php');
+                                        include('db/conexao.php');
                                         
 
                                         if(isset($_POST['email']) || isset($_POST['senha'])) {
@@ -129,69 +133,69 @@
                         </div>
 
                         <div id="box-noticia-3-grid">
-                            <a href="../PaginaDeConteudo/materia-LogisticaReversa/LogisticaReversa.php">
+                            <a href="PaginaDeConteudo/materia-LogisticaReversa/LogisticaReversa.php">
                                 <div class="box-noticia-4">
                                     <div class="box-imagem-noticia">
-                                        <img src="../PaginaDeConteudo/materia-noticias/noticias-logistica-reversa.png" alt="Logística Reversa, tudo sobre o assunto" class="imagem-noticia">
+                                        <img src="PaginaDeConteudo/materia-noticias/noticias-logistica-reversa.png" alt="Logística Reversa, tudo sobre o assunto" class="imagem-noticia">
                                         <div class="imagem-noticia-sombra"> </div>
                                     </div>
                                     <p class="noticias-letra"> Logística Reversa, tudo sobre o assunto </p>
-                                    <p class="ler-agora"> Ler agora <img src="../imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
+                                    <p class="ler-agora"> Ler agora <img src="imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
                                 </div>
                             </a>
 
-                            <a href="../PaginaDeConteudo/materia-Reciclagem/Reciclagem.php">
+                            <a href="PaginaDeConteudo/materia-Reciclagem/Reciclagem.php">
                                 <div class="box-noticia-4">
                                     <div class="box-imagem-noticia">
-                                        <img src="../PaginaDeConteudo/materia-noticias/noticias-reciclagem.png" alt="A Importância da Reciclagem" class="imagem-noticia">
+                                        <img src="PaginaDeConteudo/materia-noticias/noticias-reciclagem.png" alt="A Importância da Reciclagem" class="imagem-noticia">
                                         <div class="imagem-noticia-sombra"> </div>
                                     </div>
                                     <p class="noticias-letra"> A Importância da Reciclagem </p>
-                                    <p class="ler-agora"> Ler agora <img src="../imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
+                                    <p class="ler-agora"> Ler agora <img src="imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
                                 </div>
                             </a>
 
-                            <a href="../PaginaDeConteudo/materia-TiposDeLixo/TiposDeLixo.php">
+                            <a href="PaginaDeConteudo/materia-TiposDeLixo/TiposDeLixo.php">
                                 <div class="box-noticia-4">
                                     <div class="box-imagem-noticia">
-                                        <img src="../PaginaDeConteudo/materia-noticias/noticias-tipos-de-lixo.png" alt="Quais são os tipos de lixo?" class="imagem-noticia">
+                                        <img src="PaginaDeConteudo/materia-noticias/noticias-tipos-de-lixo.png" alt="Quais são os tipos de lixo?" class="imagem-noticia">
                                         <div class="imagem-noticia-sombra"> </div>
                                     </div>
                                     <p class="noticias-letra"> Quais são os tipos de lixo? </p>
-                                    <p class="ler-agora"> Ler agora <img src="../imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
+                                    <p class="ler-agora"> Ler agora <img src="imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
                                 </div>
                             </a>
 
-                            <a href="../PaginaDeConteudo/materia-DescarteDeLixoEletronico/DescarteLixoEletronico.php">
+                            <a href="PaginaDeConteudo/materia-DescarteDeLixoEletronico/DescarteLixoEletronico.php">
                                 <div class="box-noticia-4">
                                     <div class="box-imagem-noticia">
-                                        <img src="../PaginaDeConteudo/materia-noticias/noticias-lixo-eletronico.png" alt="Capivaras são animais considerados em perigo de extinção" class="imagem-noticia">
+                                        <img src="PaginaDeConteudo/materia-noticias/noticias-lixo-eletronico.png" alt="Capivaras são animais considerados em perigo de extinção" class="imagem-noticia">
                                         <div class="imagem-noticia-sombra"> </div>
                                     </div>
                                     <p class="noticias-letra"> Lixo Eletrônico e seus problemas para o meio ambiente </p>
-                                    <p class="ler-agora"> Ler agora <img src="../imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
+                                    <p class="ler-agora"> Ler agora <img src="imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
                                 </div>
                             </a>
 
-                            <a href="../PaginaDeConteudo/materia-CarroEletrico/CarroEletrico.php">
+                            <a href="PaginaDeConteudo/materia-CarroEletrico/CarroEletrico.php">
                                 <div class="box-noticia-4">
                                     <div class="box-imagem-noticia">
-                                        <img src="../PaginaDeConteudo/materia-noticias/noticias-carro-eletrico.png" alt="Capivaras são animais considerados em perigo de extinção" class="imagem-noticia">
+                                        <img src="PaginaDeConteudo/materia-noticias/noticias-carro-eletrico.png" alt="Capivaras são animais considerados em perigo de extinção" class="imagem-noticia">
                                         <div class="imagem-noticia-sombra"> </div>
                                     </div>
                                     <p class="noticias-letra"> Carro Elétrico vale a Pena </p>
-                                    <p class="ler-agora"> Ler agora <img src="../imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
+                                    <p class="ler-agora"> Ler agora <img src="imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
                                 </div>
                             </a>
 
                             <a href="#">
                                 <div class="box-noticia-4">
                                     <div class="box-imagem-noticia">
-                                        <img src="../PaginaDeConteudo/materia-noticias/noticias-aquecimento-global.PNG" alt="Os problemas gerados pelo Aquecimento Global" class="imagem-noticia">
+                                        <img src="PaginaDeConteudo/materia-noticias/noticias-aquecimento-global.PNG" alt="Os problemas gerados pelo Aquecimento Global" class="imagem-noticia">
                                         <div class="imagem-noticia-sombra"> </div>
                                     </div>
                                     <p class="noticias-letra"> Os problemas gerados pelo Aquecimento Global </p>
-                                    <p class="ler-agora"> Ler agora <img src="../imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
+                                    <p class="ler-agora"> Ler agora <img src="imagem/seta-ler-noticia.png" alt="" class="seta-ler-artigo"> </p>
                                 </div>
                             </a>
                         </div>
@@ -205,8 +209,8 @@
         </div>
 
         <div id="box-footer">
-            <?php include '../MenuFooter/footer/footer.html';?>
+            <?php include 'MenuFooter/footer/footer.html';?>
         </div>
     </body>
-    <script src="../MenuFooter/menu/menu.js"></script>
+    <script src="MenuFooter/menu/menu.js"></script>
 </html>
